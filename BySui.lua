ebbdb98a2fbcfdb638b68330a48e9dda2d783bb3
sh.kirito <147661025+ZenitsuScripter@users.noreply.github.com>
2025-10-03 22:20:19 -0300

    Update BySui.lua

--- BySui.lua
+++ BySui.lua
@@ -21,7 +21,7 @@ local Window = Fluent:CreateWindow({
 -- ADICIONA ABAS
 -- ================================
 local Tabs = {
-    Raid = Window:AddTab({ Title = "Raid", Icon = "star" }),
+    Raid = Window:AddTab({ Title = "Raid", Icon = "eye" }),
     PlayerTeleport = Window:AddTab({ Title = "Teleport", Icon = "eye" })
 }
 
