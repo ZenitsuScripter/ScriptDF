b8294b51fcccabb9284c17802761eda9e34fb485
sh.kirito <147661025+ZenitsuScripter@users.noreply.github.com>
2025-10-05 13:46:24 -0300

    Update Sui.lua

--- Sui.lua
+++ Sui.lua
@@ -84,7 +84,7 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 local MultiDropdown = Tabs.Boss:AddDropdown("BossDropdown", {
     Title = "Bosses disponíveis",
     Description = "Selecione os bosses que deseja farmar",
-    Values = {"Kaigaku", "Boss2", "Boss3", "Boss4", "Boss5"},
+    Values = {"Kaigaku", "GenericSlayer", "GenericOni", "Zenitsu", "GreenDemon"},
     Multi = true,
     Default = {"Kaigaku"}
 })
